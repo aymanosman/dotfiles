871344388c0fe434d87673be7c6afea34b3cf980
Ayman Osman <aymano.osman@gmail.com>
2026-04-10 12:13:48 +0100

    add org-modern
    - clean org styling, hidden markers, pretty entities
    - agenda grid and time styling
    - fringe and divider face cleanup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -485,3 +485,36 @@
   (evil-define-key 'normal dired-mode-map
     (kbd "TAB") 'dired-subtree-toggle
     (kbd "S-TAB") 'dired-subtree-remove))
+
+(progn ;; org-modern
+  (install 'org-modern)
+
+  (set-face-attribute 'org-modern-symbol nil :family "Iosevka")
+
+  (dolist (face '(window-divider
+                  window-divider-first-pixel
+                  window-divider-last-pixel))
+    (face-spec-reset-face face)
+    (set-face-foreground face (face-attribute 'default :background)))
+  (set-face-background 'fringe (face-attribute 'default :background))
+
+  (setq org-auto-align-tags nil
+        org-tags-column -67
+        org-catch-invisible-edits 'show-and-error
+        org-special-ctrl-a/e t
+        org-insert-heading-respect-content t
+
+        org-hide-emphasis-markers t
+        org-pretty-entities t
+        org-ellipsis "…"
+
+        org-agenda-tags-column 'auto
+        org-agenda-block-separator ?─
+        org-agenda-time-grid
+        '((daily today require-timed)
+          (800 1000 1200 1400 1600 1800 2000)
+          " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+        org-agenda-current-time-string
+        "◀── now ─────────────────────────────────────────────────")
+
+  (global-org-modern-mode 1))
